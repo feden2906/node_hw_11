4c41460a2336ff469dbb39023eb946e69d88dd47
Fedenko Maksym <feden2906@gmail.com>
2021-03-23 12:15:53 +0200

    default port

--- configs/configs.js
+++ configs/configs.js
@@ -2,7 +2,7 @@ module.exports = {
   MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/users_cars',
   JWT_SECRET: process.env.JWT_SECRET || 'MY_SECRET',
   JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET || 'MY_REFRESH_SECRET',
-  PORT: process.env.PORT || 6004,
+  PORT: process.env.PORT || 6000,
 
   ROOT_EMAIL: process.env.ROOT_EMAIL || 'qqq@gmail.com',
   ROOT_EMAIL_PASSWORD: process.env.ROOT_EMAIL_PASSWORD || 'qqq',
